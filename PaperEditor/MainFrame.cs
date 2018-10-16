e66133686d8634d301225be928cdcdd6cfffd9d4
Michael Engelskirchen <michael.eng@hotmail.de>
2018-10-16 15:53:47 +0200

    Rename Standard Windows

--- PaperEditor/MainFrame.cs
+++ PaperEditor/MainFrame.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PaperViewer
+namespace PaperEditor
 {
-    public partial class Form1 : Form
+    public partial class MainFrame : Form
     {
-        public Form1()
+        public MainFrame()
         {
             InitializeComponent();
         }
--- PaperViewer/MainFrame.Designer.cs
+++ PaperViewer/MainFrame.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace GameMaster
+﻿namespace PaperViewer
 {
-    partial class Form1
+    partial class MainFrame
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,17 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 771);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
